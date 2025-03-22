0cac51bf1cd797fe73e7faff69cb43249215d629
Lukas <43455793+lukasseklir@users.noreply.github.com>
2025-03-22 12:26:15 -0700

    feat: boilerplate

---
+++ BattleRoyale/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BattleRoyale
+//
+//  Created by Lukas Seklir on 3/22/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
