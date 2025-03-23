bf4a974aedc2c0ee58a4b59c97ab4f6cf8fd5c88
Lukas <43455793+lukasseklir@users.noreply.github.com>
2025-03-23 10:50:23 -0700

    feat: camo homescreen

--- BattleRoyale/ViewControllers/InitialViewController.swift
+++ BattleRoyale/ViewControllers/InitialViewController.swift
@@ -9,8 +9,7 @@ import UIKit
 
 class InitialViewController: UIViewController {
 
-    let titleLabel = UILabel.createLabel(fontSize: 40, color: .white, thickness: .heavy, numLines: 0, text: "Let's\nBattle")
-    let getStartedButton = StandardButton(title: "Get Started", tintColor: .systemIndigo, backgroundColor: .white)
+    let titleLabel = UILabel.createLabel(fontSize: 40, color: .white, thickness: .heavy, numLines: 0, text: "Battle Royale")
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,28 +18,45 @@ class InitialViewController: UIViewController {
     }
     
     func setupUI() {
-        self.view.backgroundColor = .systemIndigo
-        let stackView = UIStackView.createStackView(axis: .vertical, distribution: .equalSpacing)
+        self.view.backgroundColor = .white
+        
+        let backgroundImageView = UIImageView()
+        backgroundImageView.contentMode = .scaleAspectFill
+        backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(backgroundImageView)
+        NSLayoutConstraint.activate([
+            backgroundImageView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            backgroundImageView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            backgroundImageView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            backgroundImageView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+        ])
+        backgroundImageView.image = UIImage(named: "camo")
+        
+        let stackView = UIStackView.createStackView(axis: .vertical, spacing: 20, alignment: .center)
         stackView.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(stackView)
         NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: self.view.topAnchor),
             stackView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 32),
             stackView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -32),
-            stackView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor)
+            stackView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
         ])
         
-        let topView = UIView()
-        titleLabel.textAlignment = .center
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        topView.addSubview(titleLabel)
-        NSLayoutConstraint.activate([
-            titleLabel.leadingAnchor.constraint(equalTo: topView.leadingAnchor),
-            titleLabel.trailingAnchor.constraint(equalTo: topView.trailingAnchor),
-            titleLabel.centerYAnchor.constraint(equalTo: topView.centerYAnchor)
-        ])
+        let backgroundColor = UIColor(
+            red: CGFloat(0xF1) / 255.0,  // 241
+            green: CGFloat(0xC9) / 255.0,  // 201
+            blue: CGFloat(0x95) / 255.0,  // 149
+            alpha: 1.0
+        )
         
-        stackView.addArrangedSubview(topView)
+        let textColor = UIColor(
+            red: CGFloat(0x22) / 255.0,  // 34
+            green: CGFloat(0x28) / 255.0,  // 40
+            blue: CGFloat(0x24) / 255.0,  // 36
+            alpha: 1.0
+        )
+        
+        let getStartedButton = StandardButton(title: "Let's Battle", tintColor: textColor, backgroundColor: backgroundColor)
+        getStartedButton.widthAnchor.constraint(equalToConstant: 300).isActive = true
         
         let getStartedTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(getStartedButtonTapped(_:)))
         getStartedTapGestureRecognizer.cancelsTouchesInView = false
@@ -48,6 +64,8 @@ class InitialViewController: UIViewController {
         getStartedTapGestureRecognizer.delaysTouchesEnded = true
         getStartedButton.addGestureRecognizer(getStartedTapGestureRecognizer)
         
+        titleLabel.textAlignment = .center
+        stackView.addArrangedSubview(titleLabel)
         stackView.addArrangedSubview(getStartedButton)
     }
     
